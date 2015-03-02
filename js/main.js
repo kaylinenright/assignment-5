370ea9ec6e66a89fd1816ad252eb336b7d68e190
Kay-babycakes <kaylin.enright@hotmail.com>
2015-03-02 16:26:35 -0500

    fixed glitch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var $form = $("form");
-var $item = $(".item");
+var $item = $("#item");
 var $add = $(".add");
 var $button = $(".addbtn");
 
@@ -9,7 +9,7 @@ $form.on("submit", function (e) {
     var $addValue = $add.val();
     var $li = $("<li>");
     var $addH2 = $("<h2>").html($item.val());
-    var $button = ("<button>");
+    var $button = $("<button>");
     
     $button.html('&#x2717');
     $button.addClass("done");
